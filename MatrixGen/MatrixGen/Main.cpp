4aae578aaeb737b78393a49fde9fcf365511b182
Mateusz Lew <lewmateush@gmail.com>
2019-11-30 17:39:31 +0100

    Empty array created.

--- MatrixGen/MatrixGen/Main.cpp
+++ MatrixGen/MatrixGen/Main.cpp
@@ -8,6 +8,18 @@ int main()
 	cout << "Number of rows: "; cin >> rows;
 	cout << "Number of columns: "; cin >> cols;
 
+	int **tab = new int*[rows];
+	for (int i = 0; i < rows; i++)
+	{
+		tab[i] = new int[cols];
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] tab[i];
+	}
+	delete[] tab;
+
 	system("pause");
 	return 0;
 }
