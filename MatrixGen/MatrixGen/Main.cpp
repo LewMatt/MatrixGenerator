dbdd76be7b06757d4b251c6fb592d51d607fd137
Mateusz Lew <lewmateush@gmail.com>
2019-11-30 18:56:28 +0100

    Added a typo checker.

--- MatrixGen/MatrixGen/Main.cpp
+++ MatrixGen/MatrixGen/Main.cpp
@@ -5,40 +5,64 @@ using namespace std;
 int main()
 {
 	cout << "|||  MATRIX GENERATOR  |||" << endl << endl;
-
 	while (true)
 	{
 		int rows, cols;
 		cout << "Number of rows: "; cin >> rows;
-		cout << "Number of columns: "; cin >> cols;
-		cout << endl;
-
-		int** tab = new int* [rows];
-		for (int i = 0; i < rows; i++)
+		if(!cin)
 		{
-			tab[i] = new int[cols];
-		}
-
-		srand(time(NULL));
-		for (int i = 0; i < rows; i++)
-		{
-			for (int j = 0; j < cols; j++)
-			{
-				tab[i][j] = rand() % 20 - 10;
-				cout.width(3);
-				cout << right << tab[i][j] << " ";
-			}
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
 			cout << endl;
+			cout << "Invalid input, try again." << endl<<endl;
 		}
-		cout << endl;
-
-		for (int i = 0; i < rows; i++)
+		else
 		{
-			delete[] tab[i];
+			cout << "Number of columns: "; cin >> cols;
+			if (!cin)
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << endl;
+				cout << "Invalid input, try again." << endl<<endl;
+			}
+			else
+			{
+				if (rows >= 0 && cols >= 0)
+				{
+					cout << endl;
+					int** tab = new int* [rows];
+					for (int i = 0; i < rows; i++)
+					{
+						tab[i] = new int[cols];
+					}
+					srand(time(NULL));
+					for (int i = 0; i < rows; i++)
+					{
+						for (int j = 0; j < cols; j++)
+						{
+							tab[i][j] = rand() % 20 - 10;
+							cout.width(3);
+							cout << right << tab[i][j] << " ";
+						}
+						cout << endl;
+					}
+					cout << endl;
+					for (int i = 0; i < rows; i++)
+					{
+						delete[] tab[i];
+					}
+					delete[] tab;
+				}
+				else
+				{
+					cout << endl;
+					cout << "Invalid input, try again." << endl << endl;
+				}
+			}
 		}
-		delete[] tab;
 	}
-	
+
 	system("pause");
 	return 0;
 }
