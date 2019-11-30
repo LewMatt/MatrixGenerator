4d6ff1206d94f0a80e3caf7e5d0dedee898ff83d
Mateusz Lew <lewmateush@gmail.com>
2019-11-30 17:30:55 +0100

    Program initialized.

---
+++ MatrixGen/MatrixGen/Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int rows, cols;
+	cout << "|||  MATRIX GENERATOR  |||" << endl << endl;
+	cout << "Number of rows: "; cin >> rows;
+	cout << "Number of columns: "; cin >> cols;
+
+	system("pause");
+	return 0;
+}
